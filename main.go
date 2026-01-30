efdc3d0200244e8c78fa6a66a40a748a0410379a
Bart Jeukendrup <bart@delta10.nl>
2026-01-30 22:45:04 +0100

    Update main

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Hello World!!")
 }
 
 func main() {
 	http.HandleFunc("/", helloHandler)
-	
+
 	port := ":8080"
 	log.Printf("Server starting on port %s", port)
-	
+
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
 	}
